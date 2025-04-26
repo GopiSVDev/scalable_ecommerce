1a6ce2f8e63bbb1d0c9f936537928e6fc9324844
GopiWebDev <gopisv.webdev@gmail.com>
2025-04-26 23:14:56 +0530

    Add SLF4J logging in UserService to track login, registration, and profile fetching actions.

--- user-service/src/main/java/com/gopiwebdev/ecommerce/user_service/controller/UserController.java
+++ user-service/src/main/java/com/gopiwebdev/ecommerce/user_service/controller/UserController.java
@@ -6,12 +6,16 @@ import com.gopiwebdev.ecommerce.user_service.dto.UserResponse;
 import com.gopiwebdev.ecommerce.user_service.service.JwtService;
 import com.gopiwebdev.ecommerce.user_service.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
@@ -30,14 +34,24 @@ public class UserController {
     @Autowired
     private JwtService jwtService;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/me")
-    public ResponseEntity<UserResponse> getCurrentUser() {
+    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
         UserResponse user = service.getCurrentUser();
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("You are not logged in");
+        }
+
+        if (userDetails != null)
+            logger.info("Fetching profile for user: {}", userDetails.getUsername());
         return ResponseEntity.ok(user);
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest request) {
+        logger.info("Register request received for email: {}", request.getEmail());
         service.registerUser(request);
 
         Map<String, Object> response = new HashMap<>();
@@ -50,6 +64,7 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        logger.info("Login request received for username: {}", request.getUsername());
         Authentication authentication = manager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
 
         if (authentication.isAuthenticated()) {
