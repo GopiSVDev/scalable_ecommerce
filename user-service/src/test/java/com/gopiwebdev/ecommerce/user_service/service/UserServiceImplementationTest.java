b2dfbf7b31cebd8721db79fa3474c439557ef0b1
GopiWebDev <gopisv.webdev@gmail.com>
2025-04-29 18:34:51 +0530

    feat: Add unit test for successful user register

--- user-service/src/test/java/com/gopiwebdev/ecommerce/user_service/service/UserServiceImplementationTest.java
+++ user-service/src/test/java/com/gopiwebdev/ecommerce/user_service/service/UserServiceImplementationTest.java
@@ -1,10 +1,12 @@
 package com.gopiwebdev.ecommerce.user_service.service;
 
 import com.gopiwebdev.ecommerce.user_service.dto.RegisterRequest;
+import com.gopiwebdev.ecommerce.user_service.entity.User;
 import com.gopiwebdev.ecommerce.user_service.exception.UsernameAlreadyExistsException;
 import com.gopiwebdev.ecommerce.user_service.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -38,5 +40,26 @@ class UserServiceImplementationTest {
         });
 
         verify(repository, never()).save(any());
+    }
+
+    @Test
+    void testRegisterUser_Success() {
+        RegisterRequest request = new RegisterRequest("testuser@gmail.com", "testuser", "testpassword");
+
+        when(repository.existsByUsername(request.getUsername())).thenReturn(false);
+
+        when(passwordEncoder.encode(request.getPassword())).thenReturn("encodedPassword");
+
+        userService.registerUser(request);
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        verify(repository, times(1)).save(userCaptor.capture());
+
+        User savedUser = userCaptor.getValue();
+        assertEquals("testuser", savedUser.getUsername());
+        assertEquals("testuser@gmail.com", savedUser.getEmail());
+        assertEquals("encodedPassword", savedUser.getPassword());
+
+        verify(passwordEncoder, times(1)).encode(request.getPassword());
     }
 }
