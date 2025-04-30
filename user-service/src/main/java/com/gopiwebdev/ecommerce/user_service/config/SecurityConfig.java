67814c93354b5a76be190ac2cd0501ca30527eca
GopiWebDev <gopisv.webdev@gmail.com>
2025-04-30 14:55:25 +0530

    fix: load environment variables via Docker Compose instead of Dotenv config class

--- user-service/src/main/java/com/gopiwebdev/ecommerce/user_service/config/SecurityConfig.java
+++ user-service/src/main/java/com/gopiwebdev/ecommerce/user_service/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
 public class SecurityConfig {
 
