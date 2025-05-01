58a2111251c00aea3e71560d3c31d8ceb426d6a9
GopiWebDev <gopisv.webdev@gmail.com>
2025-05-01 17:40:56 +0530

    Dockerized all services and databases; configured service registry and API gateway
    - Containerized user-service, product-service, cart-service, and databases (PostgreSQL).
    - Set up Eureka service registry and integrated with all services.
    - Configured API Gateway (Spring Cloud Gateway) to route requests and handle service discovery.
    - Updated Docker Compose setup to ensure correct networking and service startup order.
    - Added necessary health checks and environment configurations for proper service communication.

--- api-gateway/src/main/java/com/gopiwebdev/ecommerce/api_gateway/config/EnvConfig.java
+++ api-gateway/src/main/java/com/gopiwebdev/ecommerce/api_gateway/config/EnvConfig.java
@@ -1,12 +1,14 @@
-package com.gopiwebdev.ecommerce.api_gateway.config;
+//package com.gopiwebdev.ecommerce.api_gateway.config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class EnvConfig {
+//    static {
+//        Dotenv dotenv = Dotenv.configure().load();
+//        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//    }
+//}
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class EnvConfig {
-    static {
-        Dotenv dotenv = Dotenv.configure().load();
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-    }
-}
+// comment out for local usage
