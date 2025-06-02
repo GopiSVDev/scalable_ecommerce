a596f5a8c64f5a6a6f746cc7b022ceb2f506d1ab
GopiWebDev <gopisv.webdev@gmail.com>
2025-06-02 17:11:08 +0530

    comment env config

--- api-gateway/src/main/java/com/gopiwebdev/ecommerce/api_gateway/config/EnvConfig.java
+++ api-gateway/src/main/java/com/gopiwebdev/ecommerce/api_gateway/config/EnvConfig.java
@@ -3,12 +3,12 @@ package com.gopiwebdev.ecommerce.api_gateway.config;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class EnvConfig {
-    static {
-        Dotenv dotenv = Dotenv.configure().directory("../").load();
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-    }
-}
+//@Configuration
+//public class EnvConfig {
+//    static {
+//        Dotenv dotenv = Dotenv.configure().directory("../").load();
+//        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//    }
+//}
 
 // comment out for local usage
